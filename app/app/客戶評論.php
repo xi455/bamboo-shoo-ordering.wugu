83d166e6eef1975e5494f812446ec79af4554ebd
xi455 <79513741+xi455@users.noreply.github.com>
2021-02-26 12:30:07 +0800

    Add files via upload

---
+++ app/app/客戶評論.php
@@ -0,0 +1,16 @@
+<?php
+
+//$db = new PDO('mysql:host=localhost;dbname=new', 'root', '');
+//$db->exec('set names utf8');
+
+//$sql ='INSERT INTO `user`(`name`, `b`) VALUES (:name, :b)';
+        //$query=$db->prepare($sql);
+
+      //  $query->bindValue('name',$_POST['name']);
+      //  $query->bindValue('b',$_POST['b']);
+        
+      //  $query->execute();
+      //  $user =$query->fetch(PDO::FETCH_ASSOC);
+        
+      //  header('location: 客戶評論2.php');
+        
